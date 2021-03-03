01d97ca6f7b4a7cf6d506affd503da1f897bc646
Artem <art-do@yandex.ru>
2021-03-04 00:03:36 +0400

    improved transition to login page

--- test_main_page.py
+++ test_main_page.py
@@ -3,15 +3,13 @@ from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.should_be_login_link()
-
-
-def test_guest_can_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = MainPage(browser, link) 
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    
+
+    
 
     
