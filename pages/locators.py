13a7800e31c63e8759984196d2bc5abb63d3484d
Artem <art-do@yandex.ru>
2021-03-09 21:30:37 +0400

    product page added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PAGE_ITEM_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
+    BASKET_ITEM_NAME = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PAGE_ITEM_PRICE =(By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
+    BASKET_ITEM_PRICE =(By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_item_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+
+    def should_be_the_same_name(self):
+        page_item_name = self.browser.find_element(*ProductPageLocators.PAGE_ITEM_NAME)
+        basket_item_name = self.browser.find_element(*ProductPageLocators.BASKET_ITEM_NAME)
+        assert page_item_name.text == basket_item_name.text, "The names are not the same"
+        
+    def should_be_the_same_price(self):
+        page_item_price = self.browser.find_element(*ProductPageLocators.PAGE_ITEM_PRICE)
+        basket_item_price = self.browser.find_element(*ProductPageLocators.BASKET_ITEM_PRICE)
+        assert page_item_price.text == basket_item_price.text, "The prices are not the same"
+        
+    
